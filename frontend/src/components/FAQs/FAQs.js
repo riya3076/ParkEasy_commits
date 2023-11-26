93064a19da4ce4dbb31b8ecbec01eac7d5d1dbcd
parshva <pr371441@dal.ca>
2023-11-26 01:56:09 -0400

    FAQs page

--- frontend/src/components/FAQs/FAQs.js
+++ frontend/src/components/FAQs/FAQs.js
@@ -18,19 +18,19 @@ const FAQs = () => {
 
   const faqData = [
     {
-      category: 'General',
+      category: 'Drivers',
       questions: [
         {
-          question: 'What are your business hours?',
-          answer: 'Our business hours are from 9:00 AM to 5:00 PM, Monday to Friday.',
+          question: 'What are the parking rates?',
+          answer: 'All parking rates are posted by the parking spot owner. We help guide owners in posting rates that are appropriate for the area and generally cheaper than the rates charged at public parking.',
         },
         {
-          question: 'Do you offer refunds?',
-          answer: 'Yes, we offer refunds before 30 days of travel date. Please refer to our refund policy for more details.',
+          question: 'How do I pay for renting a space?',
+          answer: 'Once you find your desired parking spot, you can select the book now option and it will direct you to the payment site and then you can enter your credit/debit card details for the payment.',
         },
         {
-          question: 'How do I update my account information?',
-          answer: 'To update your account information, log into your account and go to the "Account Settings" page. From there, you can edit your personal details, shipping address, and payment information.',
+          question: 'Can I extend my stay?',
+          answer: 'You may be able to extend your stay. Please contact the spot owner with your request.',
         },
         {
           question: 'How can I provide feedback on your products or services?',
@@ -39,47 +39,47 @@ const FAQs = () => {
       ],
     },
     {
-      category: 'Technical',
+      category: 'Parking Spot Owners',
       questions: [
         {
-          question: 'How do I reset my password?',
-          answer: 'To reset your password, go to the login page and click on the "Forgot Password" link. Follow the instructions sent to your registered email.',
+          question: 'How do I rent my parking spot?',
+          answer: 'Once you have registered an account with Parking For Me, click on List Your Spot button from the main menu and follow the instructions to enter your parking spot information. Once you have submitted your address, a ParkEasy staff member will either approve your listing or contact you if any additional information is required. Once your spot is listed, it will be available for rent.',
         },
         {
-          question: 'What browsers do you support?',
-          answer: 'We support the latest versions of Chrome, Firefox, Safari, and Edge.',
+          question: 'When do I receive my payments and how do I get paid?',
+          answer: 'It is the owners responsibility to check the payment details with the customer. Each customer shall be solely responsible for checking the details before sharing any confidential information and getting into a contract. The ParkEasy management will not accept responsibility for any loss or damage. We recommend not to accept cashier/certified checks or money orders. Also do not give out financial info (bank account, social security, PayPal account, etc.) unless you are 100% sure that it is not a scam.',
         },
         {
-          question: 'How do I clear my browser cache?',
-          answer: 'To clear your browser cache, go to the browser settings or preferences menu and look for the "Clear browsing data" option. Select the cache or temporary files option and click on Clear or Delete.',
+          question: 'How much does Parking For Me charge for listing my spot?',
+          answer: 'Nothing! Listing is completely free. The rates that you choose to charge for your parking spot are the amounts you will receive when someone books your space.',
         },
         {
-          question: 'How do I troubleshoot login problems?',
-          answer: 'If you are experiencing login problems, first double-check your username and password. If the issue persists, try resetting your password or contacting our support team for assistance.',
+          question: 'How do I get parking spot access information to a driver?',
+          answer: 'Whenever a renter will be interested in your parking spot, he/she will send you a request via our online contact form. You can then respond to that email or contact the renter on the phone number provided in the email and give them the detailed instructions. To preserve privacy, we will not display your personal information online.',
         },
       ],
     },
-    {
-      category: 'Third party attributions',
-      questions: [
-        {
-          question: 'Are there any ongoing promotions?',
-          answer: 'Yes, we currently have a summer sale with discounts up to 40% off on selected packages.',
-        },
-        {
-          question: 'How can I redeem a discount code?',
-          answer: 'During the checkout process, you can enter your discount code in the designated field and click "Apply" to redeem it.',
-        },
-        {
-          question: 'Are there any upcoming sales or promotions?',
-          answer: 'We frequently run sales and promotions throughout the year. To stay updated, sign up for our newsletter or follow us on social media',
-        },
-        {
-          question: 'Can I combine multiple discount codes?',
-          answer: 'In general, our system allows the use of only one discount code per order. Multiple discount codes cannot be combined.',
-        },
-      ],
-    },
+    // {
+    //   category: 'Third party attributions',
+    //   questions: [
+    //     {
+    //       question: 'Are there any ongoing promotions?',
+    //       answer: 'Yes, we currently have a summer sale with discounts up to 40% off on selected packages.',
+    //     },
+    //     {
+    //       question: 'How can I redeem a discount code?',
+    //       answer: 'During the checkout process, you can enter your discount code in the designated field and click "Apply" to redeem it.',
+    //     },
+    //     {
+    //       question: 'Are there any upcoming sales or promotions?',
+    //       answer: 'We frequently run sales and promotions throughout the year. To stay updated, sign up for our newsletter or follow us on social media',
+    //     },
+    //     {
+    //       question: 'Can I combine multiple discount codes?',
+    //       answer: 'In general, our system allows the use of only one discount code per order. Multiple discount codes cannot be combined.',
+    //     },
+    //   ],
+    // },
   ];
 
   return (
