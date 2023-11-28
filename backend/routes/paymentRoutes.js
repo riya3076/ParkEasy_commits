def5ba00b4df5d5ae761e006a2e922749fa25cbf
Riya <ry470536@dal.ca>
2023-11-28 15:24:18 -0400

    get payments backend done

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -4,6 +4,6 @@ const payController = require('../controllers/payController');
 const router = express.Router();
 
 router.post('/', payController.payment);
-
+router.get("/:userName/payments", payController.getPayments); 
 
 module.exports = router;
